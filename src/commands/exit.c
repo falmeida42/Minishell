db0992bb52cedf9414d4cc33c1d59a93b89b8c33
jpceia <joao.p.ceia@gmail.com>
2021-11-18 11:01:30 +0000

    moved ft_exit function to different file

--- src/commands/exit.c
+++ src/commands/exit.c
@@ -1,33 +1,26 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   pwd.c                                              :+:      :+:    :+:   */
+/*   exit.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: jpceia <joao.p.ceia@gmail.com>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/09/03 18:35:42 by falmeida          #+#    #+#             */
-/*   Updated: 2021/11/18 11:00:41 by jpceia           ###   ########.fr       */
+/*   Created: 2021/11/18 11:00:45 by jpceia            #+#    #+#             */
+/*   Updated: 2021/11/18 11:00:56 by jpceia           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-/**
- * Print the absolute  pathname of the current working directory. 
- * The return status is 0 unless an error occurs while reading the name of the
- * current directory or an invalid option is supplied.
+/*
+ * Cause the shell to exit with a status of n.
+ * If n is omitted, the exit status is that of the last command executed.
+ * A trap on EXIT is executed before the shell terminates.
  */
-
-char	*pwd(void)
+void	ft_exit(char *input)
 {
-	char	*str;
-
-	str = NULL;
-	str = getcwd(str, PATH_MAX);
-	return (str);
-}
-
-void	ft_pwd(void)
-{
-	printf("%s\n", pwd());
+	free_struct(input);
+	g_mini.exit = true;
+	ft_putstr("exit\n");
+	// kill(g_mini.pid, SIGINT);
 }
