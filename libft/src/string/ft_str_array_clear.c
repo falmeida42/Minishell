4a1d377984766b4782489a8ffce122cb8ae421c1
jpceia <joao.p.ceia@gmail.com>
2021-11-24 22:54:38 +0000

    ft_str_array_clear for null terminated array

--- libft/src/string/ft_str_array_clear.c
+++ libft/src/string/ft_str_array_clear.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_str_array_clear.c                               :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jceia <jceia@student.42.fr>                +#+  +:+       +#+        */
+/*   By: jpceia <joao.p.ceia@gmail.com>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/01 18:29:45 by jceia             #+#    #+#             */
-/*   Updated: 2021/09/09 02:54:50 by jceia            ###   ########.fr       */
+/*   Updated: 2021/11/19 18:41:21 by jpceia           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,10 +18,21 @@ void	ft_str_array_clear(char **str_arr, int len)
 	int	index;
 
 	index = 0;
-	while (index < len)
+	if (len > 0)
 	{
-		free(str_arr[index]);
-		index++;
+		while (index < len)
+		{
+			free(str_arr[index]);
+			index++;
+		}
+	}
+	else
+	{
+		while(str_arr[index])
+		{
+			free(str_arr[index]);
+			index++;
+		}
 	}
 	free(str_arr);
 }
