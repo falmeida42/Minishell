cbb9d2c2396b0baf24029d97fed6cd5196497ac0
Joao Ceia <jceia@student.42lisboa.com>
2021-11-29 16:05:59 +0000

    wip

--- src/parser/command_parser.c
+++ src/parser/command_parser.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   command_parser.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jpceia <joao.p.ceia@gmail.com>             +#+  +:+       +#+        */
+/*   By: jceia <jceia@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/18 10:35:15 by jpceia            #+#    #+#             */
-/*   Updated: 2021/11/29 12:26:44 by jpceia           ###   ########.fr       */
+/*   Updated: 2021/11/29 15:54:26 by jceia            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -39,62 +39,22 @@ void	simple_command_parse_redirection_token(
 	}
 }
 
-t_simple_command	*simple_command_parse(t_token_iterator *it)
+t_simple_command	*simple_command_parse(t_token_list *it)
 {
 	t_simple_command	*cmd;
 	t_token				*token;
 
 	cmd = ft_calloc(1, sizeof(*cmd));
-	while (*it)
+	while (it)
 	{
-		token = (*it)->content;
+		token = it->content;
 		if (is_word_token(token))
 			ft_lstpush_back(&cmd->argv, ft_strdup(token->value));
 		else if (is_redirection_token(token))
-			simple_command_parse_redirection_token(cmd, it);
+			simple_command_parse_redirection_token(cmd, &it);
 		else
 			break ;
-		token_iterator_next(it);
+		it = it->next;
 	}
 	return (cmd);
-}
-
-bool	command_tree_parse_step(t_token_iterator *it, t_command_tree **command)
-{
-	t_token		*token;
-	static bool	prev_pipe = true;
-
-	token = (*it)->content;
-	if (is_simple_command_token(token))
-	{
-		// ft_lstpush_back(command, simple_command_parse(it));
-		prev_pipe = false;
-		return (true);
-	}
-	else if (token->type == TOKEN_PIPE)
-	{
-		if (prev_pipe)
-		{
-			// piped_command_free(*command);
-			*command = NULL;
-			ft_putstr_error("syntax error near unexpected token '|'\n");
-			return (false);
-		}
-		token_iterator_next(it);
-		prev_pipe = true;
-		return (true);
-	}
-	return (false);
-}
-
-t_command_tree	*command_tree_parse(t_token_iterator *it)
-{
-	t_command_tree	*command;
-	bool			do_continue;
-
-	command = NULL;
-	do_continue = true;
-	while (*it && do_continue)
-		do_continue = command_tree_parse_step(it, &command);
-	return (command);
 }
--- src/parser/parser.c
+++ src/parser/parser.c
@@ -3,14 +3,16 @@
 /*                                                        :::      ::::::::   */
 /*   parser.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jpceia <joao.p.ceia@gmail.com>             +#+  +:+       +#+        */
+/*   By: jceia <jceia@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/16 01:47:24 by jpceia            #+#    #+#             */
-/*   Updated: 2021/11/29 12:27:37 by jpceia           ###   ########.fr       */
+/*   Updated: 2021/11/29 16:04:36 by jceia            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
+
+
 
 /*
  * Parses a group of commands within parentheses.
@@ -40,6 +42,8 @@ t_command_tree	*command_tree_unwrap_parse(t_token_iterator *it)
  */
 t_command_tree	*command_tree_node_parse(t_token_iterator *it)
 {
+	int					parent_count;
+	t_token_list		*tmp;
 	t_token				*token;
 	t_ast_node			*node;
 
@@ -51,6 +55,25 @@ t_command_tree	*command_tree_node_parse(t_token_iterator *it)
 	node = ast_node_new(AST_CMD);
 	if (!node)
 		return (NULL);
+	tmp = *it;
+	while (tmp)
+	{
+		token = *tmp;
+		if (token->type == TOKEN_LPAREN)
+			parent_count++;
+		else if (token->type == TOKEN_RPAREN)
+			parent_count--;
+		else if (token->type == TOKEN_AND || token->type == TOKEN_AND)
+		{
+			if (parent_count == 0)
+				break ;
+		}
+		tmp = tmp->next;
+	}
+	// if breaks in logical operator
+	if (token->type == TOKEN_AND || token->type == TOKEN_AND)
+	// else 
+	if (token->type)
 	node->cmd = simple_command_parse(it);
 	return (btree_create_node(node));
 }
@@ -85,21 +108,96 @@ t_command_tree	*command_tree_right_parse(t_token_iterator *it,
 	return (ast);
 }
 
-/*
- * Parses a group of commands (with parenthesis or && or || separators)
- */
-t_command_tree	*command_tree_parse(t_token_iterator *it)
+
+t_command_tree	*command_tree_parse_split_on(t_token_iterator *it, t_token *token, t_token *eot)
 {
-	t_command_tree	*left_ast;
+	t_token_type	type;
+	t_ast_node	*node;
 
 	if (!it || !*it)
 		return (NULL);
-	left_ast = command_tree_node_parse(it);
-	if (!left_ast)
+	type = token->type;
+	if (type ==)
+		node = ast_node_new();
+}
+
+t_token	*token_list_lookup_logical(t_token_list *lst)
+{
+	int		parent_count;
+	t_token	*token;
+
+	parent_count = 0;
+	while (lst)
+	{
+		token = (t_token *)lst->content;
+		if (token->type == TOKEN_LPAREN)
+			parent_count++;
+		else if (token->type == TOKEN_RPAREN)
+			parent_count--;
+		else if (token->type == TOKEN_AND || token->type == TOKEN_OR
+			|| parent_count == 0)
+			return (token);
+		lst = lst->next;
+	}
+	return (NULL);
+}
+
+t_token	*token_list_lookup_pipe(t_token_list *lst)
+{
+	int		parent_count;
+	t_token	*token;
+
+	parent_count = 0;
+	while (lst)
+	{
+		token = (t_token *)lst->content;
+		if (token->type == TOKEN_LPAREN)
+			parent_count++;
+		else if (token->type == TOKEN_RPAREN)
+			parent_count--;
+		else if (token->type == TOKEN_PIPE && parent_count == 0)
+			return (token);
+		lst = lst->next;
+	}
+	return (NULL);
+}
+
+t_command_tree	*command_tree_parse_simple_command(t_token_list *it)
+{
+	t_ast_node			*ast;
+	
+	ast = ast_node_new(AST_CMD);
+	if (!ast)
 		return (NULL);
-	if (!*it)
-		return (left_ast);
-	return (command_tree_right_parse(it, left_ast));
+	ast->cmd = simple_command_parse(it);
+	return (btree_create_node(ast));
+}
+
+/*
+ * Parses a group of commands (with parenthesis or && or || separators)
+ */
+t_command_tree	*command_tree_parse_until(t_token_list *it, t_token *eot)
+{
+	t_token				*token;
+
+	if (!it || !*it)
+		return (NULL);
+	token = (*it)->content;
+	if (token->type == TOKEN_AND || token->type == TOKEN_OR || token->type == TOKEN_PIPE)
+		return (NULL); // invalid syntax
+	token = token_list_lookup_logical(*it, eot);
+	if (token)
+		return (command_tree_parse_split_on(it, token, eot));
+	token = token_list_lookup_pipe(*it);
+	if (token)
+		return (command_tree_parse_split_on(it, token, eot));
+	token = (*it)->content;
+	if (token->type == TOKEN_LPAREN)
+	{
+		token_iterator_next(it);
+		return (command_tree_parse(it));
+	}
+	return (command_tree_parse_simple_command(it));
 }
 
 t_command_tree	*parser(char *input)
@@ -111,7 +209,7 @@ t_command_tree	*parser(char *input)
 	token_list = lex(input);
 	// apply expander
 	token_it = token_list;
-	ast = command_tree_parse(&token_it);
+	ast = command_tree_parse_until(token_it, NULL);
 	ft_lstclear(&token_list, token_free);
 	return (ast);
 }
