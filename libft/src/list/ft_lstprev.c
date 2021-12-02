52b1b8fd0486d481711b888fefbb8f05c0a024fb
jpceia <joao.p.ceia@gmail.com>
2021-12-02 09:15:13 +0000

    added ft_lstprev to libft

---
+++ libft/src/list/ft_lstprev.c
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstprev.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jpceia <joao.p.ceia@gmail.com>             +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/12/02 08:57:06 by jpceia            #+#    #+#             */
+/*   Updated: 2021/12/02 09:15:02 by jpceia           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+t_list	*ft_lstprev(t_list *lst, void *data)
+{
+	t_list	*prev;
+
+	if (!lst)
+		return (NULL);
+	prev = lst;
+	while (prev && prev->next->content != data)
+		prev = prev->next;
+	return (prev);
+}
