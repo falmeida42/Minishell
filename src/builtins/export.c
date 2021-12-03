8d1298e9dcf2ce9387c1dd0e46308cc72110d681
jpceia <joao.p.ceia@gmail.com>
2021-12-03 18:37:23 +0000

    added export function with no or multiple args. Misses sort in env variables (no args)

--- src/builtins/export.c
+++ src/builtins/export.c
@@ -6,7 +6,7 @@
 /*   By: jpceia <joao.p.ceia@gmail.com>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/07 11:41:30 by fferreir          #+#    #+#             */
-/*   Updated: 2021/11/25 02:30:13 by jpceia           ###   ########.fr       */
+/*   Updated: 2021/12/03 18:35:01 by jpceia           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,22 +22,45 @@
  * one of the names is not a valid shell variable name.
  */
 
+// Prints the map to the console.
+void	ft_export_no_args(void)
+{
+	t_map	*it;
+	t_pair	*p;
+
+	it = g_mini.env;
+	while (it)
+	{
+		p = (t_pair *)it->content;
+		ft_putstr("declare -x ");
+		ft_putstr(p->key);
+		ft_putstr("=\"");
+		ft_putstr(p->value);
+		ft_putstr("\"\n");
+		it = it->next;
+	}
+}
+
 /**
  * Reimplementation of the builtin export
  * Adds or edits an environment variable, stored at g_mini.env
  */
 int	ft_export(char **argv)
 {
-	char	*arg;
 	t_pair	*p;
 
-	arg = argv[1];
-	if (arg == NULL)
-		return (1);
-	p = pair_from_str(arg, '=');
-	if (!p)
-		return (1);
-	env_set(p->key, p->value);
-	free(p);
+	if (!argv[1])
+	{
+		ft_export_no_args();
+		return (0);
+	}
+	while(*(++argv))
+	{
+		p = pair_from_str(*argv, '=');
+		if (!p)
+			return (1);
+		env_set(p->key, p->value);
+		free(p);	
+	}
 	return (0);
 }
