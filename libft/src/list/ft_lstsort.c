c46b259256d9f2ded4d37da24e352479d84e1ce8
jpceia <joao.p.ceia@gmail.com>
2021-12-04 09:51:38 +0000

    added ft_lstsort

---
+++ libft/src/list/ft_lstsort.c
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstsort.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jpceia <joao.p.ceia@gmail.com>             +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/12/03 18:40:26 by jpceia            #+#    #+#             */
+/*   Updated: 2021/12/04 09:46:30 by jpceia           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	ft_lstsort(t_list **begin_list, int (*cmp)(void *, void *))
+{
+	t_list	*l;
+	t_list	*l_prev;
+	t_list	*holder;
+
+	l = *begin_list;
+	l_prev = NULL;
+	while (l->next)
+	{
+		if (cmp(l->content, l->next->content) < 0)
+		{
+			holder = l->next;
+			l->next = l->next->next;
+			holder->next = l;
+			if (l_prev)
+				l_prev->next = holder;
+			else
+				*begin_list = holder;
+			ft_lstsort(begin_list, cmp);
+			break ;
+		}
+		l_prev = l;
+		l = l->next;
+	}
+}
