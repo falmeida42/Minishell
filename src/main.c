44b77648e8bcc6e1d13bf086e38dc2a9b99ab096
jpceia <joao.p.ceia@gmail.com>
2021-12-06 16:35:19 +0000

    added non interactive shell

--- src/main.c
+++ src/main.c
@@ -3,40 +3,73 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jceia <jceia@student.42.fr>                +#+  +:+       +#+        */
+/*   By: jpceia <joao.p.ceia@gmail.com>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/03 18:35:11 by falmeida          #+#    #+#             */
-/*   Updated: 2021/12/03 16:36:20 by jceia            ###   ########.fr       */
+/*   Updated: 2021/12/06 16:34:48 by jpceia           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 #include "libft.h"
+#include <fcntl.h>
+
+int	minishell_non_interactive(char *fname)
+{
+	int	fd;
+	int	status;
+
+	fd = open(fname, O_RDONLY);
+	if (fd < 0)
+	{
+		perror("minishell");
+		return (EXIT_FAILURE);
+	}
+	status = 1;
+	while (status > 0)
+	{
+		status = ft_get_next_line(fd, &g_mini.input);
+		g_mini.tree = parser(g_mini.input);
+		if (g_mini.input && ft_strwc(g_mini.input, ' ') > 0)
+			add_history(g_mini.input);
+		if (g_mini.parse_error)
+			ft_putendl_error(g_mini.parse_error);
+		else if (g_mini.tree)
+			g_mini.status = command_tree_execute(g_mini.tree);
+		mini_loop_clear(&g_mini);
+		if (g_mini.exit)
+			break ;
+	}
+	if (close(fd))
+	{
+		perror("minishell");
+		return (EXIT_FAILURE);
+	}
+	if (status < 0)
+		return (EXIT_FAILURE);
+	return (EXIT_SUCCESS);
+}
 
 int main(int argc, char **argv, char **envp)
 {
-	(void) argc;
-	(void) argv;
 	g_mini.env = map_from_str_array(envp, '=');
 	g_mini.exit = false;
 	g_mini.parse_error = NULL;
 
+	if (argc > 1)
+		return (minishell_non_interactive(argv[1]));
 	//signal(SIGINT , get_signal);
 	//signal(SIGQUIT , get_signal);
 	while (42)
 	{
 		g_mini.input = readline("minishell: ");
 		g_mini.tree = parser(g_mini.input);
+		if (g_mini.input && ft_strwc(g_mini.input, ' ') > 0)
+			add_history(g_mini.input);
 		if (g_mini.parse_error)
-		{
-			add_history(g_mini.input);
 			ft_putendl_error(g_mini.parse_error);
-		}
 		else if (g_mini.tree)
-		{
-			add_history(g_mini.input);
 			g_mini.status = command_tree_execute(g_mini.tree);
-		}
 		mini_loop_clear(&g_mini);
 		if (g_mini.exit)
 		{
