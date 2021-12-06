9c1cfef8b7b1c8ffa19f2f3326d248111d1be1c0
jpceia <joao.p.ceia@gmail.com>
2021-12-06 18:55:47 +0000

    added ft_lstinsert_at

---
+++ libft/src/list/ft_lstinsert_at.c
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstinsert_at.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jpceia <joao.p.ceia@gmail.com>             +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/12/06 17:20:54 by jpceia            #+#    #+#             */
+/*   Updated: 2021/12/06 17:38:56 by jpceia           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+t_list	*ft_lstinsert_at(t_list *node, void *content)
+{
+    t_list	*node_new;
+    
+    node_new = ft_lstnew(content);
+    if (!node_new)
+        return (NULL);
+    if (node->next)
+        node_new->next = node->next;
+    node->next = node_new;
+    return (node_new);
+}
