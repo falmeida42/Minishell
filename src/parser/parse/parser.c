e4c52760fa1f62e5bc1912c44c2c0a14893698b8
jpceia <joao.p.ceia@gmail.com>
2021-12-07 19:44:49 +0000

    removed  lex_and_expand function

--- src/parser/parse/parser.c
+++ src/parser/parse/parser.c
@@ -3,43 +3,14 @@
 /*                                                        :::      ::::::::   */
 /*   parser.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jceia <jceia@student.42.fr>                +#+  +:+       +#+        */
+/*   By: jpceia <joao.p.ceia@gmail.com>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/16 01:47:24 by jpceia            #+#    #+#             */
-/*   Updated: 2021/12/07 15:09:09 by jceia            ###   ########.fr       */
+/*   Updated: 2021/12/07 18:25:04 by jpceia           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
-
-char	**lex_and_expand(char *input)
-{
-	char			**arr;
-	int				index;
-	int				size;
-	t_token			*token;
-	t_token_list	*token_list;
-	t_token_list	*token_it;
-
-	token_list = lex(input);
-	ft_lstmap(token_list, expand_operation, token_free);
-	size = ft_lstsize(token_list);
-	arr = (char **)malloc(sizeof(*arr) * (size + 1));
-	if (!arr)
-		return (NULL);
-	index = 0;
-	token_it = token_list;
-	while (token_it)
-	{
-		token = token_it->content;
-		if (is_word_token(token))
-			arr[index++] = ft_strdup(token->value);
-		token_it = token_it->next;
-	}
-	arr[index] = NULL;
-	ft_lstclear(&token_list, token_free);
-	return (arr);
-}
 
 int	is_empty_word_token_wrap(void *ptr)
 {
