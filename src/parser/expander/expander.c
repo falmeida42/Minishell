73dcc48506f3f0e260077852a88b72874ef80dd8
jpceia <joao.p.ceia@gmail.com>
2021-12-08 09:20:33 +0000

    removed needs_to_replace_dollar function

--- src/parser/expander/expander.c
+++ src/parser/expander/expander.c
@@ -6,7 +6,7 @@
 /*   By: jpceia <joao.p.ceia@gmail.com>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/14 18:29:58 by jpceia            #+#    #+#             */
-/*   Updated: 2021/12/07 19:38:12 by jpceia           ###   ########.fr       */
+/*   Updated: 2021/12/08 08:51:55 by jpceia           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,50 +30,25 @@ void	*expand_operation(void *content)
 	return (token);
 }
 
-int	needs_to_replace_dollar(char *str)
-{
-	int	i;
-
-	i = 0;
-	while (str[i])
-	{
-		if (str[i] == '$'
-			&& !(ft_contains(str[i + 1], " \t") || str[i + 1] == '\0'))
-			return (1);
-		i++;
-	}
-	return (0);
-}
-
 char	*ft_expander(char *str)
 {
-	char	*result;
 	int		i;
 
 	if (!str)
 		return (NULL);
 	i = 0;
-	result = str;
-	if (!needs_to_replace_dollar(str))
-		return (str);
 	while (str[i] != '\0')
 	{
 		if (str[i] == '$')
 		{
 			if (str[i + 1] == '?')
-				result = replace_dollar_status(str, i);
+				return (ft_expander(replace_dollar_status(str, i)));
 			else if (str[i + 1] == '{')
-				result = replace_dollar_brackets(str, i);
-			else if (ft_contains(str[i + 1], " \"") || str[i + 1] == '\0')
-			{
-				i++ ;
-				continue ;
-			}
-			else
-				result = replace_dollar_generic(str, i);
-			break ;
+				return (ft_expander(replace_dollar_brackets(str, i)));
+			else if (!(ft_contains(str[i + 1], " \"") || str[i + 1] == '\0'))
+				return (ft_expander(replace_dollar_generic(str, i)));
 		}
 		i++;
 	}
-	return (ft_expander(result));
+	return (str);
 }
