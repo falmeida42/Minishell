86c71c27cea4a3de39546384beca0cb9a7f6189d
Joao Ceia <jceia@student.42lisboa.com>
2021-12-09 17:47:17 +0000

    updated take_dquotes to avoid invalid writes

--- src/parser/lexer/lexer_utils.c
+++ src/parser/lexer/lexer_utils.c
@@ -6,37 +6,30 @@
 /*   By: jceia <jceia@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/14 17:52:49 by jpceia            #+#    #+#             */
-/*   Updated: 2021/12/09 16:49:44 by jceia            ###   ########.fr       */
+/*   Updated: 2021/12/09 17:46:49 by jceia            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 #include <stdlib.h>
 
-char	*remove_quotes(char *str)
+void	*remove_quotes(char *str)
 {
 	char	*str2;
+	int		len;
 	int		i;
 	int		j;
 
-	str2 = malloc(ft_strlen(str) + 1);
+	len = ft_strlen(str);
+	str2 = malloc(len + 1);
 	i = 0;
 	j = 0;
 	while (str[i] != '\0')
 	{
-		while (str[i] == '"')
-		{
-			if (i > 0 && str[i - 1] == '\\')
-			{
-				str2[j] = str[i];
-				i++;
-				j++;
-			}
+		if (str[i] == '"' && (i == 0 || str[i-1] != '\\'))
 			i++;
-		}
-		str2[j] = str[i];
-		i++;
-		j++;
+		else
+			str2[j++] = str[i++];
 	}
 	str2[j] = '\0';
 	free(str);
@@ -49,32 +42,31 @@ t_token	*take_dquoted(char **cursor)
 	char	prev_char;
 	char	*start;
 	char	*end;
-	int		len;
+	char 	*aux;
+	bool	inside_quotes;
 
+	inside_quotes = true;
 	start = *cursor;
 	char_iterator_next(cursor);
 	c = char_iterator_peek(cursor);
-	len = 1;
 	prev_char = 0;
 	end = NULL;
 	while (c)
 	{
 		if (c == '"' && prev_char != '\\')
-			len++;
-		if (len % 2 == 0)
+			inside_quotes = !inside_quotes;
+		else if (c == ' ' && !inside_quotes)
 		{
-			if (*(*cursor + 1) == ' ')
-			{
-				end = *cursor;
-				char_iterator_next(cursor);
-				break ;
-			}
+			end = *cursor;
+			break ;
 		}
 		prev_char = c;
 		c = char_iterator_next(cursor);
 	}
-	start = ft_substr(start, 0, end - (start - 1));
-	return (token_new(TOKEN_DQUOTED, remove_quotes(start)));
+	if (end == NULL)
+		end = *cursor;
+	aux = ft_substr(start, 0, end - start);
+	return (token_new(TOKEN_DQUOTED, remove_quotes(aux)));
 }
 
 t_token	*take_quoted(char **cursor)
