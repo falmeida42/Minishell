c7e9627e847b8827f000426ee8c6b3b718b1cd56
Joao Ceia <jceia@student.42lisboa.com>
2021-12-13 13:01:24 +0000

    replicating bash

--- src/builtins/unset.c
+++ src/builtins/unset.c
@@ -6,7 +6,7 @@
 /*   By: jceia <jceia@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/07 11:48:40 by fferreir          #+#    #+#             */
-/*   Updated: 2021/12/13 12:50:52 by jceia            ###   ########.fr       */
+/*   Updated: 2021/12/13 13:01:09 by jceia            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,11 +20,7 @@
 int	ft_unset(char **argv, int fd)
 {
 	(void)fd;
-	if (!argv[1])
-	{
-		ft_putendl_error("unset: not enough arguments");
-		return (1);
-	}
-	env_unset(argv[1]);
+	if (argv[1])
+		env_unset(argv[1]);
 	return (0);
 }
